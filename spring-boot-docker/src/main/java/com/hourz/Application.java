efe076329401f3f3c27b6eab2060f2bab704d0ac
hourz <hourongzhen@casic27.com>
2019-01-14 15:12:14 +0800

    spring boot2.0+docker集成简单小例子

--- spring-boot-docker/src/main/java/com/hourz/Application.java
+++ spring-boot-docker/src/main/java/com/hourz/Application.java
@@ -1,12 +1,12 @@
-package com.neo;
+package com.hourz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DockerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DockerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
--- spring-boot-docker/src/test/java/com/hourz/ApplicationTests.java
+++ spring-boot-docker/src/test/java/com/hourz/ApplicationTests.java
@@ -1,14 +1,13 @@
-package com.neo;
+package com.hourz;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DockerApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
