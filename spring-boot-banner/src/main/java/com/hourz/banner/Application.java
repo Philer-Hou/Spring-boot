08e73e023edfbe666902cd2006be388c33f50391
hourz <hourongzhen@casic27.com>
2019-01-14 14:25:28 +0800

    spring boot 2.0+admin+banner简单小例子

--- spring-boot-banner/src/main/java/com/hourz/banner/Application.java
+++ spring-boot-banner/src/main/java/com/hourz/banner/Application.java
@@ -1,26 +1,19 @@
-package com.hourz;
+package com.hourz.banner;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 /**
- * <p>Admin Server服务端端</p>
+ * <p>Admin Banner服务</p>
  * @author hourz
- * @since 2019-01-11
+ * @since 2019-01-14
  */
-@Configuration
-@EnableAutoConfiguration
-@EnableAdminServer //服务端
 @SpringBootApplication
 public class Application {
 
-	private static final Logger logger = LogManager.getLogger(Application.class); 
+	private static final Logger logger = LogManager.getLogger(Application.class);
 	
 	public static void main(String[] args) {
 		logger.info("Admin服务端-监控中心----服务启动！");
